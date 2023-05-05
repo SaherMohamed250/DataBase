eb2529fbcf19b860cca8ccfdbc57c0520b3db556
SaherMohamed250 <SaherMohamed250>
2023-05-05 23:54:21 +0300

    sa

---
+++ src/main/java/com/cu/sci/Test.java
@@ -0,0 +1,27 @@
+package com.cu.sci;
+
+import com.cu.sci.Dao.DBEmployee;
+import com.cu.sci.models.Employee;
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+
+        DBEmployee db= new DBEmployee();
+//        for(Employee employee : db.get()){
+//            System.out.println(employee);
+//        }
+//        System.out.println(db.get(101));
+        Employee employee = new Employee();
+        employee.setFirstName("Saher");
+        employee.setLastName("Mohamed");
+        employee.setEmail("saher@gmail.com");
+        employee.setJobId("SH_CLERK");
+        employee.setSalary(16000);
+        employee.setDepartmentId(50);
+        employee.setHireDate(new Date());
+
+        System.out.println(db.insert(employee));
+    }
+}
